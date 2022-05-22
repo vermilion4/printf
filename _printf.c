294cb8c46128d84e146be28f932a5cbbea0708ac
Adaeze Ndupu <andupu4@gmail.com>
2022-05-22 20:50:14 +0100

    Test 0

--- _printf.c
+++ _printf.c
@@ -1,8 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
-
-int print_format(const char *format, ...);
 
 /**
  * _printf - produces output according to a format
@@ -11,37 +8,41 @@ int print_format(const char *format, ...);
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i;
+va_list args;
+int i;
 
-    va_start(args, format);
-    
-    i = 0;
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                print_char(args);
-            }
-            else if (format[i] == 's')
-            {
-                print_string(args);
-            }
-            else
-            {
-                _putchar('%');
-                _putchar(format[i]);
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-        }
-        i++;
-    }
-    va_end(args);
-    return (i);
+va_start(args, format);
+
+i = 0;
+while (format[i] != '\0')
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == 'c')
+{
+print_char(args);
+}
+else if (format[i] == 's')
+{
+print_string(args);
+}
+else if (format[i] == '%')
+{
+_putchar('%');
+}
+else
+{
+_putchar('%');
+_putchar(format[i]);
+}
+}
+else
+{
+_putchar(format[i]);
+}
+i++;
+}
+va_end(args);
+return (i);
 }
